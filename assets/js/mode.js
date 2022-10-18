5e1ee9a081a933be17186203c443c0e99fef92c8
kunilovsky <kunilovsky@gmail.com>
2022-10-18 11:21:53 +0500

    Animation Mode Icon. No Active item list dropdown

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -8,7 +8,9 @@ const darkThemes = document.getElementById('darkThemes');
 const lightThemes = document.getElementById('lightThemes');
 const systemThemes = document.getElementById('systemThemes');
 
-const icons = document.querySelector('opendrop');
+/* Анимированные элементы и эффекты */
+
+const icons = document.getElementById('iconTheme');
 const burger = document.getElementById('burger');
 const drop = document.getElementById('drop-black');
 
@@ -26,25 +28,38 @@ darkThemes.addEventListener('click', () => {
 systemThemes.addEventListener('click', () => {
   if (matchMedia('(prefers-color-scheme: light)').matches) {
     document.body.classList.remove('dark');
+    icons.classList.remove('bi-moon-fill-1');
+    icons.classList.add('bi-brightness-high-fill-1');
   } else {
     window.localStorage.setItem('memoryTheme', 'dark');
   }
   if (matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.classList.add('dark');
+    icons.classList.remove('bi-brightness-high-fill-1');
+    icons.classList.add('bi-moon-fill-1');
   } else {
     window.localStorage.setItem('memoryTheme', 'light');
   }
 });
 
 /* Проверяем локальное хранилище посетителя при входе на сайт */
+
 if (memoryTheme === 'dark') {
   document.body.classList.add('dark');
+  icons.classList.add('bi-moon-fill-1');
+  icons.classList.remove('bi-brightness-high-fill-1');
 }
+
+/* Активный item */
 
 /* Выбор темы */
 
 lightThemes.addEventListener('click', () => {
   document.body.classList.add('light');
+  if (icons.classList.contains('bi-moon-fill-1')) {
+    icons.classList.remove('bi-moon-fill-1');
+    icons.classList.add('bi-brightness-high-fill-1');
+  }
   if (document.body.classList.contains('dark')) {
     document.body.classList.remove('dark');
   }
@@ -52,12 +67,16 @@ lightThemes.addEventListener('click', () => {
 
 darkThemes.addEventListener('click', () => {
   document.body.classList.add('dark');
+  if (icons.classList.contains('bi-brightness-high-fill-1')) {
+    icons.classList.remove('bi-brightness-high-fill-1');
+    icons.classList.add('bi-moon-fill-1');
+  }
   if (document.body.classList.contains('light')) {
     document.body.classList.remove('light');
   }
 });
 
-/* Вывод в консоль */
+/* Вывод в консоль (dev) */
 
 const themeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
